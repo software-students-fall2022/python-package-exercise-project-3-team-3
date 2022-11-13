11ed2f3609400e3314edfcdeb831c0d38a88403b
Pedro <pbaggio765@gmail.com>
2022-11-13 13:22:00 -0500

    Update version

--- src/pyrecommendg3/__main__.py
+++ src/pyrecommendg3/__main__.py
@@ -11,16 +11,37 @@ def main():
     print("5. Exit")
 
     answer = int(input("Enter your choice (1-5): "))
-    print()
+    
 
     if answer == 5:
       break
     elif answer == 1:
-      print(recommendations.get_music_recommendation())
+      artist = input("Enter artist (leave empty if you want from any artist): ")
+      genre = input("Enter genre (leave empty if you want from any genre): ")
+      if artist == "":
+        artist = None
+      if genre == "":
+        genre = None
+      print(recommendations.get_music_recommendation(artist=artist, genre=genre))
     elif answer == 2:
-      print(recommendations.get_movie_recommendation())
+      director = input("Enter director (leave empty if you want from any director): ")
+      genre = input("Enter genre (leave empty if you want from any genre): ")
+      rating = input("Enter rating (PG-13, PG, etc.) (leave empty if you want from any rating): ")
+      if director == "":
+        director = None
+      if genre == "":
+        genre = None
+      if rating == "":
+        rating = None
+      print(recommendations.get_movie_recommendation(director=director, genre=genre, rating=rating))
     elif answer == 3:
-      print(recommendations.get_food_recommendation())
+      cuisine = input("Enter cuisine (leave empty if you want from any cuisine): ")
+      allergen = input("Enter allergen (leave empty if you don't want to consider allergens): ")
+      if cuisine == "":
+        cuisine = None
+      if allergen == "":
+        allergen = None
+      print(recommendations.get_food_recommendation(cuisine=cuisine, allergen=allergen))
     elif answer == 4:
       print("1. View all music")
       print("2. View all movies")
